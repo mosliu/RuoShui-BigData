6237e3ae16af78e7177ed9744f3087b5c1f17693
admin <2217343704@qq.com>
2023-06-26 09:29:08 +0800

    初始化

--- ruoshui-system/src/main/java/com/ruoshui/test/Sqltest.java
+++
@@ -1,7 +0,0 @@
-package com.ruoshui.test;
-
-public class Sqltest {
-    public static void main(String[] arg){
-        System.out.println("SELECT table_name AS TABLENAME, table_comment AS TABLECOMMENT FROM information_schema.tables where table_schema = '\" + dbName + \"' \"");
-    }
-}
